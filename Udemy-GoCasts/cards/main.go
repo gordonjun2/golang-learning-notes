29fa637e02a2512e3a0eb49482db3a400652e7da
gordonjun <gordonoh@yahoo.com.sg>
2023-04-20 19:03:15 +0800

    continued with Udemy

---
+++ Udemy-GoCasts/cards/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := newDeck()
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	remainingCards.print()
+
+	fmt.Println(hand.toString())
+
+	hand.saveToFile("cards_on_hand")
+
+}
