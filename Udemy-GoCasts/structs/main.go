3c0cc6cc103889f724e85737d125d9b92a01ff0b
gordonjun <gordonoh@yahoo.com.sg>
2023-04-21 15:15:48 +0800

    continue with Udemy

---
+++ Udemy-GoCasts/structs/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	// var alex person
+
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	// jimPointer := &jim
+	jim.updateName("Jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
